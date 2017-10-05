e9872c0ddbc3368ac66012c0b40f74161dc9d3c5
Manfred Berndtgen <mberndtgen@gmail.com>
2017-10-05 09:43:40 +0200

    update ewww configuration

--- init.el
+++ init.el
@@ -153,16 +153,13 @@
 (global-set-key (kbd "H-<") "~")
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(custom-safe-themes (quote ("5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" "52588047a0fe3727e3cd8a90e76d7f078c9bd62c0b246324e557dfa5112e0d0c" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(fci-rule-color "#2a2a2a")
  '(scroll-preserve-screen-position t)
  '(which-key-mode t))
+
 (electric-indent-mode +1) ;; indent after entering RET
 (electric-pair-mode +1) ;; automatically add a closing paren
 (desktop-save-mode 1) ;; save sessions
@@ -171,6 +168,10 @@
       '((:eval (if (buffer-file-name)
                    (abbreviate-file-name (buffer-file-name))
                  "%b"))))
+;; make eww default browser
+(setq browse-url-browser-function 'eww-browse-url)
+
+
 
 (defun find-user-init-file () ;; instant access to init file
   "Edit the `user-init-file', in another window."
